e0a6bdf268cdcf1a006dad4b038d9f4cd0b02230
couldi <dudals2014@gmail.com>
2018-11-13 22:55:27 +0900

    Updates

---
+++ precache-manifest.dbd81e14b4a7bdce4f5f2f855d863549.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f9521ac5b62e87334b5b",
+    "url": "/movie_app/static/js/runtime~main.f9521ac5.js"
+  },
+  {
+    "revision": "c37a6ac91db947e7ec76",
+    "url": "/movie_app/static/js/main.c37a6ac9.chunk.js"
+  },
+  {
+    "revision": "46602178259828021dee",
+    "url": "/movie_app/static/js/1.46602178.chunk.js"
+  },
+  {
+    "revision": "c37a6ac91db947e7ec76",
+    "url": "/movie_app/static/css/main.3dfc0575.chunk.css"
+  },
+  {
+    "revision": "828dba5675de949155b834075faf2363",
+    "url": "/movie_app/index.html"
+  }
+];
