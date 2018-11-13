ef11f8a136cc50704b3fe13009462cefb71b6e95
couldi <dudals2014@gmail.com>
2018-11-13 23:17:58 +0900

    css 업데이트

--- src/App.js
+++ src/App.js
@@ -5,28 +5,42 @@ import Movie from './movie';
 
 
 class App extends Component {
+	state = {};
 
-  state={}
+	componentDidMount() {
+		this._getMovies();
+	}
 
-  componentDidMount(){
-    fetch('https://yts.am/api/v2/list_movies.json?sort_by=download_count');
-  }
+	_renderMovies = () => {
+		const movies = this.state.movies.map((movie, index) => {
+      return <Movie
+      title={movie.title_english}
+      poster={movie.medium_cover_image}
+      key={index}
+      genres={movie.genres}
+      synopsis={movie.synopsis}
+    />;
+		});
+		return movies;
+	};
 
+	_getMovies = async () => {
+    const movies = await this._callApi();
+    this.setState({
+      movies
+    });
+	};
 
-  _renderMovies = () => {
-    const movies = this.state.movies.map((movie, index) => {
-          return <Movie title={movie.title} poster={movie.poster} key={index}/>
-        })
-    return movies
-  }
+	_callApi = () => {
+		return fetch('https://yts.am/api/v2/list_movies.json?sort_by=download_count')
+			.then((response) => response.json())
+			.then((json) => json.data.movies)
+			.catch((err) => console.log(err));
+	};
 
-  render() {
-    return (
-      <div className="App">
-        {this.state.movies ? this._renderMovies() : 'Loading' }
-      </div>
-    );
-  }
+	render() {
+		return <div className="App">{this.state.movies ? this._renderMovies() : 'Loading'}</div>;
+	}
 }
 
 export default App;
